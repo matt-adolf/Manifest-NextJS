96672a3ecbc8e5d23808939f2abb4afc56acbc37
matt-adolf <74278530+matt-adolf@users.noreply.github.com>
2024-05-29 20:11:23 -0500

    Created mock document data

---
+++ src/app/mock/document.js
@@ -0,0 +1,18 @@
+export const document = {
+    "name": "test.md",
+    "path": "test.md",
+    "sha": "258aa4982d42cd8c63dbde1ce9fb4123b58dc5e1",
+    "size": 536,
+    "url": "https://api.github.com/repos/matt-adolf/Documentation/contents/test.md?ref=main",
+    "html_url": "https://github.com/matt-adolf/Documentation/blob/main/test.md",
+    "git_url": "https://api.github.com/repos/matt-adolf/Documentation/git/blobs/258aa4982d42cd8c63dbde1ce9fb4123b58dc5e1",
+    "download_url": "https://raw.githubusercontent.com/matt-adolf/Documentation/main/test.md?token=ARWWNATEDIJ6ZIWLUHZKJJLGK7GBS",
+    "type": "file",
+    "content": "LS0tCnRpdGxlOiBUZXN0IERvY3VtZW50CmF1dGhvcjogSm9obiBEb2UKZGF0\nZTogMDEtMDEtMjAyMwotLS0KIyBIZWxsbyBXb3JsZAojIyBDaGFwdGVyIDEK\nTG9yZW0gaXBzdW0gZG9sb3Igc2l0IGFtZXQsIGNvbnNlY3RldHVyIGFkaXBp\nc2NpbmcgZWxpdCwgc2VkIGRvIGVpdXNtb2QgdGVtcG9yIGluY2lkaWR1bnQg\ndXQgbGFib3JlIGV0IGRvbG9yZSBtYWduYSBhbGlxdWEuIFV0IGVuaW0gYWQg\nbWluaW0gdmVuaWFtLCBxdWlzIG5vc3RydWQgZXhlcmNpdGF0aW9uIHVsbGFt\nY28gbGFib3JpcyBuaXNpIHV0IGFsaXF1aXAgZXggZWEgY29tbW9kbyBjb25z\nZXF1YXQuIER1aXMgYXV0ZSBpcnVyZSBkb2xvciBpbiByZXByZWhlbmRlcml0\nIGluIHZvbHVwdGF0ZSB2ZWxpdCBlc3NlIGNpbGx1bSBkb2xvcmUgZXUgZnVn\naWF0IG51bGxhIHBhcmlhdHVyLiBFeGNlcHRldXIgc2ludCBvY2NhZWNhdCBj\ndXBpZGF0YXQgbm9uIHByb2lkZW50LCBzdW50IGluIGN1bHBhIHF1aSBvZmZp\nY2lhIGRlc2VydW50IG1vbGxpdCBhbmltIGlkIGVzdCBsYWJvcnVtLgo=\n",
+    "encoding": "base64",
+    "_links": {
+        "self": "https://api.github.com/repos/matt-adolf/Documentation/contents/test.md?ref=main",
+        "git": "https://api.github.com/repos/matt-adolf/Documentation/git/blobs/258aa4982d42cd8c63dbde1ce9fb4123b58dc5e1",
+        "html": "https://github.com/matt-adolf/Documentation/blob/main/test.md"
+    }
+}
