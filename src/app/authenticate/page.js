814874cbe22917f99dcad070fbc410d7f6ff4232
matt-adolf <74278530+matt-adolf@users.noreply.github.com>
2024-05-29 20:31:36 -0500

    Removed params from Authenticate page

--- src/app/authenticate/page.js
+++ src/app/authenticate/page.js
@@ -1,5 +1,5 @@
 import { redirect } from 'next/navigation';
 
-export default async function Page({ params }) {
+export default async function Page() {
     redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&scope=repo&redirect_uri=${'http://localhost:3000/authorize'}`)
 }
