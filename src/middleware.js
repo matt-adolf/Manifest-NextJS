adf2feffd0f562f5b08b0b567d18712c18294606
matt-adolf <74278530+matt-adolf@users.noreply.github.com>
2024-06-23 15:51:40 -0500

    Created middleware file

---
+++ src/middleware.js
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+
+export function middleware(req) {
+    //let response = NextResponse.next();
+    //response.cookies.set("Name", "Matt");
+
+    //const response = NextResponse.json({ authorized: true });
+
+    // response.cookies.set({
+    //     name: 'Name',
+    //     value: 'Matt',
+    //     path: '/',
+    //     sameSite: 'lax',
+    //     maxAge: (3600000 / 2), // 30 minutes
+    // });
+
+    console.log("MIDDLEWARE EXECUTED!!!")
+    //return response;
+}
+
+export const config = {
+    matcher: '/api/authenticate',
+}
